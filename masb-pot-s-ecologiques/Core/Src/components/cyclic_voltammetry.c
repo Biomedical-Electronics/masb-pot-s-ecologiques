8b1dbafb2418a5270483d8ccd9622393aaf8ce42
Helena-Riesco-Domingo <hriesco3@gmail.com>
2021-05-15 18:11:19 +0200

    fijando tensiones Vcell, vObjetivo y cerrando relé

--- masb-pot-s-ecologiques/Core/Src/components/cyclic_voltammetry.c
+++ masb-pot-s-ecologiques/Core/Src/components/cyclic_voltammetry.c
@@ -8,3 +8,15 @@
   */
 
 #include "components/cyclic_voltammetry.h"
+#include "components/stm32main.h"
+
+double vObjetivo = 0; //inicializamos la variable vObjetivo
+
+V_CELL = cvConfiguration.eBegin; // fijamos la tensión de la celda electroquímica a eBegin
+
+vObjetivo = cvConfiguration.eVertex1; // establecemos como voltaje objetivo el del vértice 1
+
+// cerramos el relé
+HAL_GPIO_WritePin(RELAY_GPIO_Port, RELAY_Pin, 1);
+
+//Aquí faltaría hacer lo de medir
