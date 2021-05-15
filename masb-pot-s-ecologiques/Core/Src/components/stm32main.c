aee93f46ddfda03da8da6ffb84ccbdf0d73c1ddd
Helena-Riesco-Domingo <hriesco3@gmail.com>
2021-05-15 16:56:33 +0200

    Creación del callback para el sampling period

--- masb-pot-s-ecologiques/Core/Src/components/stm32main.c
+++ masb-pot-s-ecologiques/Core/Src/components/stm32main.c
@@ -16,56 +16,17 @@ struct Data_S data;
 
 static TIM_HandleTypeDef *timer;
 
-void CA_setTimer(TIM_HandleTypeDef *new_Timer) {
-	timer = new_timer;
-}
-
-void CV_setTimer(TIM_HandleType *new_Timer) {
-	timer = new_timer;
-}
-
 void setup(struct Handles_S *handles) {
     MASB_COMM_S_setUart(handles->huart);
     MASB_COMM_S_waitForMessage();
+
+    HAL_TIM_Base_Start_IT(&htim3); // inicializamos el timer
 }
 
 void loop(void) {
     if (MASB_COMM_S_dataReceived()) { // Si se ha recibido un mensaje...
 
- 		switch(MASB_COMM_S_command()) { // Miramos que comando hemos recibido
-
- 			case START_CV_MEAS: // Si hemos recibido START_CV_MEAS
-
-                 // Leemos la configuracion que se nos ha enviado en el mensaje y
-                 // la guardamos en la variable cvConfiguration
-				cvConfiguration = MASB_COMM_S_getCvConfiguration();
-
-
- 				__NOP(); // Esta instruccion no hace nada y solo sirve para poder anadir un breakpoint
-
- 				// Aqui iria todo el codigo de gestion de la medicion que hareis en el proyecto
-                // si no quereis implementar el comando de stop.
-
- 				break;
-
-			case STOP_MEAS: // Si hemos recibido STOP_MEAS
-
- 				/*
- 				 * Debemos de enviar esto desde CoolTerm:
- 				 * 020300
- 				 */
- 				__NOP(); // Esta instruccion no hace nada y solo sirve para poder anadir un breakpoint
-
- 				// Aqui iria el codigo para tener la medicion si implementais el comando stop.
-
- 				break;
-
- 			default: // En el caso que se envia un comando que no exista
-
-
- 				break;
-
- 		}
+ 		HAL_TIM_PeriodElapsedCallback(&htim3);
 
        // Una vez procesado los comando, esperamos el siguiente
  		MASB_COMM_S_waitForMessage();
@@ -73,4 +34,55 @@ void loop(void) {
  	}
 
 
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+	switch(MASB_COMM_S_command()) { // Miramos que comando hemos recibido
+
+	 			case START_CV_MEAS: // Si hemos recibido START_CV_MEAS
+
+	                 // Leemos la configuracion que se nos ha enviado en el mensaje y
+	                 // la guardamos en la variable cvConfiguration
+					cvConfiguration = MASB_COMM_S_getCvConfiguration();
+
+
+	 				__NOP(); // Esta instruccion no hace nada y solo sirve para poder anadir un breakpoint
+
+	 				// Aqui iria todo el codigo de gestion de la medicion que hareis en el proyecto
+	                // si no quereis implementar el comando de stop.
+
+	 				break;
+
+	 			case START_CA_MEAS: // Si hemos recibido START_CA_MEAS
+
+					 // Leemos la configuracion que se nos ha enviado en el mensaje y
+					 // la guardamos en la variable caConfiguration
+					caConfiguration = MASB_COMM_S_getCaConfiguration();
+
+
+					__NOP(); // Esta instruccion no hace nada y solo sirve para poder anadir un breakpoint
+
+					// Aqui iria todo el codigo de gestion de la medicion que hareis en el proyecto
+					// si no quereis implementar el comando de stop.
+
+					break;
+
+				case STOP_MEAS: // Si hemos recibido STOP_MEAS
+
+	 				/*
+	 				 * Debemos de enviar esto desde CoolTerm:
+	 				 * 020300
+	 				 */
+	 				__NOP(); // Esta instruccion no hace nada y solo sirve para poder anadir un breakpoint
+
+	 				// Aqui iria el codigo para tener la medicion si implementais el comando stop.
+
+	 				break;
+
+	 			default: // En el caso que se envia un comando que no exista
+
+
+	 				break;
+
+	 		}
 }
